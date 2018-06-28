938c2d0abdab91f06c32e281cb85ace4d72ae126
dinhvandai63 <dvdai63@gmail.com>
2018-06-28 14:07:24 +0700

    Revert "Xamarin 26/6"
    This reverts commit f23a7e250b434c58bfd5610b348155fb3ba3b4d5.

---
+++ XamarinAppRauv2/XamarinAppRauv2/XamarinAppRauv2/MainMenu.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinAppRauv2
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainMenu : ContentPage
+	{
+		public MainMenu ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
