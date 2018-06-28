172722aedcc043c1bc90ec05a707d942de8634d7
dinhvandai63 <dvdai63@gmail.com>
2018-06-28 08:19:30 +0700

    app xamarin rau

---
+++ XamarinAppRauv2/XamarinAppRauv2/XamarinAppRauv2/Models/Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinAppRauv2
+{
+     public class Data
+    {
+        public string Name { get; set; }
+        public double Cost { get; set; }
+        public string ContactImage { get; set; }
+    }
+}
---
+++ XamarinAppRauv2/XamarinAppRauv2/XamarinAppRauv2/Views/MainMenu.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinAppRauv2
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainMenu : ContentPage
+	{
+		public MainMenu ()
+		{
+			InitializeComponent ();
+		}
+
+        private void ClickProduct(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Product());
+        }
+
+        private void ClickIntroduction(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Introduction());
+        }
+    }
+}
